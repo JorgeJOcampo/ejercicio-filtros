200afd8a05506add6fb46802360d97d2b67bec3d
Shorsh <jorge@cliengo.com>
2020-11-25 11:36:58 -0300

    qqq

---
+++ filters.js
@@ -0,0 +1,45 @@
+// Escribir una función que filtre una lista de contactos por una lista de filtros.
+
+const contacts = [
+  {
+    'name': 'Juan',
+    'email': 'elpuma@gmail.com',
+    'phone': '1126807183',
+    'country': 'Argentina',
+    'role': 'Backend'
+  },
+  {
+    'name': 'Marina',
+    'email': 'maru@gmail.com',
+    'phone': '1112849762',
+    'country': 'Argentina',
+    'role': 'PM'
+  },
+  {
+    'name': 'Fabricio',
+    'email': 'favric@gmail.com',
+    'phone': '1194821673',
+    'country': 'Argentina',
+    'role': 'Frontend'
+  },
+  {
+    'name': 'Jorge',
+    'email': 'jorge@gmail.com',
+    'phone': '1138491549',
+    'country': 'Argentina',
+    'role': 'Backend'
+  },
+  {
+    'name': 'Monica',
+    'email': 'moni@gmail.com',
+    'phone': '1146289715',
+    'country': 'Colombia',
+    'role': 'PM'
+  }
+]
+
+const filters = {
+  'country': 'Argentina',
+  'role': 'Backend'
+}
+
